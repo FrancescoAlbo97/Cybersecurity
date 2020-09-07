363ef07ec2a214c2ce20aadd63806b4c147e2965
FrancescoAlbo97 <fralbo97@gmail.com>
2020-09-07 12:37:52 +0200

    image ok

--- node/routes/image.js
+++ node/routes/image.js
@@ -26,50 +26,52 @@ router.get('/get', authenticateToken, async (req, res) => {
 
 //ROTTA CARICAMENTO IMMAGINE SU IPFS
 router.post('/uploadIPFS', authenticateToken, (req, res) => {
-  if(!req.files || Object.keys(req.files).length === 0){
-      res.status(400).send('No file');
-  }
-  const file = req.files.image;
-  const imagePath = './files/' + file.name;
-  let hash = "";
-  file.mv(imagePath, async (err) => {
-      if (err) {
-          console.log('Error: failed to load the file');
-          return res.status(500).send(err);
-      }
-      const f = fs.readFileSync(imagePath);
-      const fileAdded = await ipfs.add({ path: "fire", content: f });
-      hash = fileAdded.cid.toString(); //hash del file appena caricato
-      
+    if(!req.files || Object.keys(req.files).length === 0){
+        res.status(400).send('No file');
+    }
+    const file = req.files.image;
+    const imagePath = './files/' + file.name;
+    let hash = "";
+    let tags;
+    let metadata;
+    file.mv(imagePath, async (err) => {
+        if (err) {
+            console.log('Error: failed to load the file');
+            return res.status(500).send(err);
+        }
+        const f = fs.readFileSync(imagePath);
+        const fileAdded = await ipfs.add({ path: "fire", content: f });
+        hash = fileAdded.cid.toString(); //hash del file appena caricato
 
-      let fs1 = require('fs'),
-          request = require('request'),
-          apiKey = 'acc_3a4bbdbae6a16ba',
-          apiSecret = '28251a159773af4d1569790407dc44c8',
-          filePath = imagePath,
-          formData = {
-              image: fs1.createReadStream(imagePath)
-          };
+        getImageMetadata(f).then(
+            async (result) => {
+                metadata = result;
+                if(metadata){
+                    getImageData(imagePath).then(
+                        async (result) => {
+                            tags = result;
+                            await sendToBlockChain(hash, metadata, tags); 
+                        }
+                    );
+                }               
+        
+                fs.unlink(imagePath, (err) => {   //rimuovo il file appena creato
+                    if (err) console.log(err);
+                });   
+        
+                res.json({value: 'https://ipfs.io/ipfs/' + hash});
+            }
+        ).catch(
+            () => {
+                fs.unlink(imagePath, (err) => {   //rimuovo il file appena creato
+                    if (err) console.log(err);
+                });
 
-      request.post({url:'https://api.imagga.com/v2/tags', formData: formData},
-         function (error, response, body) {
-              let data = JSON.parse(body);
-              let tags = data.result.tags.slice(0, 5);
-              getImageMetadata(f).then(
-                async (metadata) => {
-                  sendBlock.send(hash, metadata, tags);        
-                  console.log("caricamento...");
-                  let images = new Object();
-                  images['gallery'] = await getBlock.getAll(); 
-                }
-              )//.catch(                res.json({hash: 'immagine non supportata'})              );
-          }).auth(apiKey, apiSecret, true);
+                res.json({value: 'error: l\'immagine non è supportata'});
+            }
+        );
 
-      fs.unlink(filePath, (err) => {   //rimuovo il file appena creato
-          if (err) console.log(err);
-      });    
-      res.json({hash: 'https://ipfs.io/ipfs/' + hash});
-  })
+    })
 })
 
 
@@ -88,21 +90,48 @@ function authenticateToken(req, res, next) {
 function getImageMetadata(filePath) {
   return new Promise(
     (resolve, reject) => {
-      try {
-        new ExifImage({ image : filePath }, function (error, exifData) {
-            if (error){
-                console.log('Error: '+error.message);
-                reject();
-            }
-            else
-              resolve(exifData);
-        });
-    } catch (error) {
-        console.log('Error: ' + error.message);
-        reject();
-    }
+        try {
+            new ExifImage({ image : filePath }, function (error, exifData) {
+                if (error){
+                    console.log('Error: '+ error.message);
+                    reject();
+                } else
+                    resolve(exifData);
+            });
+        } catch (error) {
+            console.log('Error: ' + error.message);
+            reject();
+        }
     } 
   );
+}
+
+function getImageData(imagePath) {
+    let fs1 = require('fs'),
+        request = require('request'),
+        apiKey = 'acc_3a4bbdbae6a16ba',
+        apiSecret = '28251a159773af4d1569790407dc44c8',
+        filePath = imagePath,
+        formData = {
+            image: fs1.createReadStream(imagePath)
+        };
+    return new Promise(
+      (resolve, reject) => {
+        request.post({url:'https://api.imagga.com/v2/tags', formData: formData},
+        function (error, response, body) {
+            let data = JSON.parse(body);
+            let tags = data.result.tags.slice(0, 5);
+            resolve(tags);
+      }).auth(apiKey, apiSecret, true);
+      } 
+    );
+}
+
+async function sendToBlockChain(hash, metadata, tags) {
+    sendBlock.send(hash, metadata, tags);        
+    console.log("caricamento...");
+    let images = new Object();
+    images['gallery'] = await getBlock.getAll(); 
 }
 
 module.exports = router;
